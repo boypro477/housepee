ad73ba70fca5f63e9f0ebc448ae27e37a85243d5
tu nguyen anh <nguyenanhtu0407@gmail.com>
2019-03-02 16:57:44 +0700

    edit header

--- htdocs/views/v_index.php
+++ htdocs/views/v_index.php
@@ -2,7 +2,11 @@
 require("v_header.php");
 ?>
 <div class="row">
-    <div class="col-sm-4">col-sm-4</div>
+    <div class="col-sm-4">col-sm-4
+        <div>
+            
+        </div>
+    </div>
     <div class="col-sm-8">col-sm-8</div>
 </div>
 <?php
